80332d9710f1a7758657a62323569d8dfb22f129
Benji Altman <benji@altmansoftwaredesign.com>
2021-10-07 10:56:36 -0500

    Add project files.

---
+++ tabletransfer/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace tabletransfer
+{
+	public static class TableTransfer
+	{
+		public static async Task SendAsync(Stream stream, IEnumerable<string> headers = null)
+		{
+		}
+	}
+}
